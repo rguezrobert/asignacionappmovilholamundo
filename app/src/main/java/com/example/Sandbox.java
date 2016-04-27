e583b8c4be2325a3be2e83ad8c2f03e415d94f28
rguezrobert <rguezrobert@gmail.com>
2016-04-27 15:20:36 -0430

    asiganacion hola mundo

---
+++ app/src/main/java/com/example/Sandbox.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by robert on 14-02-2016.
+ */
+public class Sandbox {
+    private List<String> mGreetings = new ArrayList<>();
+}
